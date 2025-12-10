832bc67bec549b3c2df58bc59c263be7b948afce
Lautaro Cardozo <lautarocardozo365@gmail.com>
2025-12-10 19:32:29 -0300

    Se corrigieron errores de scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,19 @@ function agregarAlCarrito() {
 contador++;
 document.getElementById("contadorCarrito").textContent = contador;
 }
+// Mostrar/Ocultar caption al tocar en mobile
+document.querySelectorAll('.carousel-item').forEach(item => {
+  item.addEventListener('click', () => {
+    const caption = item.querySelector('.carousel-caption');
+    if (!caption) return;
+    caption.classList.toggle('visible');
+  });
+});
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.menu-navegacion');
+  if (window.scrollY > 50) { // cuando bajás más de 50px
+    header.classList.add('shrink'); // ojo: corregí el typo
+  } else {
+    header.classList.remove('shrink');
+  }
+});
