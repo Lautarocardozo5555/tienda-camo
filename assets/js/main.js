af26215aafb972c1939d3132dca541ea97f09e24
Lautaro Cardozo <lautarocardozo365@gmail.com>
2025-12-11 20:10:33 -0300

    Tienda CAMO

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,37 @@ function agregarAlCarrito() {
 contador++;
 document.getElementById("contadorCarrito").textContent = contador;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".menu-navegacion");
+  if (!header) return;
+
+  const toggleShrink = () => {
+    if (window.scrollY > 50) {
+      header.classList.add("shrink");
+    } else {
+      header.classList.remove("shrink");
+    }
+  };
+
+  // Ejecutar al cargar (por si ya hay scroll inicial)
+  toggleShrink();
+
+  // Escuchar el scroll
+  window.addEventListener("scroll", toggleShrink, { passive: true });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const banners = document.querySelectorAll(".banner");
+
+  banners.forEach(banner => {
+    const captions = banner.querySelectorAll(".carousel-caption");
+
+    captions.forEach(caption => {
+    banner.addEventListener("click", () => {
+    caption.classList.toggle("visible");
+    });
+    });
+  });
+});
